241f74db2c7a18b2a9374c4544c9a78a3dcf0f17
Devin Hight <devin@deliverr.com>
2023-10-11 12:51:18 -0600

    add PUT need minor changes

--- routes/population.js
+++ routes/population.js
@@ -6,7 +6,7 @@ router.get('/state/:state/city/:city', async (req, res) => {
     const { city, state} = req.params;
 
     try {
-        const result = await db.pool.query('SELECT * FROM city_populations WHERE city = $1 AND state = $2', [city, state]);
+        const result = await db.pool.query('SELECT * FROM city_populations WHERE city ILIKE  $1 AND state ILIKE $2', [city, state]);
         if (result.rows.length > 0){
             res.status(200).json({ population: result.rows[0].population });
         } else {
@@ -18,4 +18,33 @@ router.get('/state/:state/city/:city', async (req, res) => {
     }
 });
 
+router.put('/state/:state/city/:city', async (req, res) => {
+    const { city, state } = req.params;
+    const population = parseInt(req.body);
+
+    if(isNaN(population)){
+        return res.status(400).json({ message: "Invalid population value" });
+    }
+
+    try {
+        const updateResult = await db.pool.query(`
+            UPDATE city_populations SET population = $3 
+            WHERE LOWER(city) = LOWER($1) AND LOWER(state) = LOWER($2)
+            RETURNING id
+        `, [city, state, population]);
+
+        if(updateResult.rowCount === 0){
+            await db.pool.query(`
+                INSERT INTO city_populations(city, state, population)
+                VALUES ($1, $2, $3)
+            `, [city, state, population]);
+            return res.status(201).json({ message: "Population added for new city-state combination" });
+        } else {
+            return res.status(200).json({ message: "Population updated for city-state combination" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error updating population data" });
+    }
+});
 module.exports = router;
